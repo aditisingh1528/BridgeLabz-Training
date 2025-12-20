13e8a055772c68efddb7e49856f125acf64ee8fb
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-20 11:39:38 +0530

    [Aditi] Add. Program to convert Kilometers to Miles

---
+++ core-csharp-practice/gcr-codebase/programming-elements/distanceconversion.cs
@@ -0,0 +1,10 @@
+public class distanceconversion {
+	public static void Main(string[] args){
+		//take value in kilometers  
+		int Kilometers = 25;
+		//calculate and print miles
+		double Miles = Kilometers*0.621371;
+		Console.WriteLine("Miles: "+ Miles);
+		
+	}
+}
