7f292ebf95920c7eb40970df47994e3a2b7419c3
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-20 11:35:43 +0530

    [Aditi] Add. Program to print the result of base raised to the exponent

---
+++ core-csharp-practice/gcr-codebase/programming-elements/powercalculation.cs
@@ -0,0 +1,10 @@
+public class powercalculation {
+	 static void Main(String[] args){
+		//define base value and exponent value 
+		int basee = 4;
+		int exponent = 6;
+		//calculate and print the result
+		Console.WriteLine("Power calculation : "+ Math.Pow(basee, exponent));
+	}
+
+}
