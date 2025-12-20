7a55eaecf616d67059cad729d39d51b2af6fd150
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-20 14:58:39 +0530

    [Aditi] Add. Program to find the age of Harry

---
+++ core-csharp-practice/gcr-codebase/c# programming-elements/level-1/ageOfHarry.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class ageOfHarry{
+	static void Main(){
+		//Define birth year 
+		int birthYear = 2000;
+		//Calculate and print Harry's age
+		int harryAge = 2024 - birthYear;
+		Console.WriteLine("Current Age Of Harry: " + harryAge);
+	}
+}
