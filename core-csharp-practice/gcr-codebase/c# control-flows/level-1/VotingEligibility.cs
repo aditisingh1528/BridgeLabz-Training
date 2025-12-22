06e285b6888c44adc526c5b58d1c991d872d7301
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-23 00:52:51 +0530

    [Aditi] Add. Program to check whether a person can vote based on age

---
+++ core-csharp-practice/gcr-codebase/c# control-flows/level-1/VotingEligibility.cs
@@ -0,0 +1,17 @@
+using System;
+
+class VotingEligibility{
+	
+    static void Main(){
+		//Taking input age from user
+		Console.WriteLine("Enter the age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18){
+            Console.WriteLine("The person's age is " + age + " and can vote.");
+        }
+        else{
+            Console.WriteLine("The person's age is " + age + " and cannot vote.");
+        }
+    }
+}
