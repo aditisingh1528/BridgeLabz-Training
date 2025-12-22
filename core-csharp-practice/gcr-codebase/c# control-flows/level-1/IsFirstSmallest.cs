8da2b66a02fb4ec9c609ef5f456d29719c9dc215
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-23 00:18:14 +0530

    [Aditi] Add. Program to check if the first number is the smallest of all

---
+++ core-csharp-practice/gcr-codebase/c# control-flows/level-1/IsFirstSmallest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class IsFirstSmallest{
+
+	static void Main(string[] args){
+        //Taking inputs of three numbers from user
+		Console.WriteLine("Enter the first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the third number: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        bool result = (num1 < num2 && num1 < num3);
+
+        Console.WriteLine("Is the first number the samllest? " + result);
+    }
+}
