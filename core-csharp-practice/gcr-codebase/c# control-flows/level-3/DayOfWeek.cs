cf18e8ed820863e1f0f8ed4a51d7b1ba36ed2f4e
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-23 23:04:18 +0530

    [Aditi] Add. DayOfWeek program using Gregorian Calendar formula

---
+++ core-csharp-practice/gcr-codebase/c# control-flows/level-3/DayOfWeek.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class DayOfWeek{
+    static void Main(string[] args){
+		
+        int month = int.Parse(args[0]); // month
+        int day = int.Parse(args[1]); // day
+        int year = int.Parse(args[2]); // year
+
+        int y0 = year - (14 - month) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = month + 12 * ((14 - month) / 12) - 2;
+        int d0 = (day + x + (31 * m0) / 12) % 7;
+
+        Console.WriteLine(d0);
+    }
+}
