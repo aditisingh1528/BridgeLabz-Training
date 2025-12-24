e3e5e06e1f0d2f62bc7431a7f7e8f469d7c81cb9
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-24 12:02:49 +0530

    [Aditi] Add. Storing maximum ten values in array until user enter zero or a negative number and showing their sum

---
+++ core-csharp-practice/gcr-codebase/c# arrays/level-1/StoreAndSum.cs
@@ -0,0 +1,30 @@
+using System;
+
+class StoreAndSum{
+    static void Main(){
+        double[] numbers = new double[10];
+        double total = 0.0;
+        int index = 0;
+
+        // Taking input using infinite loop
+        while (true){
+            Console.Write("Enter a number: ");
+            double input = double.Parse(Console.ReadLine());
+
+            if (input <= 0 || index == 10)
+                break;
+
+            numbers[index] = input;
+            index++;
+        }
+
+        // Display numbers and calculate sum
+        Console.WriteLine("Entered numbers: ");
+        for (int i = 0; i < index; i++){
+            Console.WriteLine(numbers[i]);
+            total += numbers[i];
+        }
+
+        Console.WriteLine("Sum of all numbers = " + total);
+    }
+}
