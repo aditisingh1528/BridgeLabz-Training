50d21bcca675590f59401fc9330e9fe67f968605
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-26 12:09:38 +0530

    [Aditi] Add. Program to display calendar for a given month and year

--- core-csharp-practice/gcr-codebase/c# strings/level-1/SimpleCalendar.cs
+++ core-csharp-practice/gcr-codebase/c# strings/level-1/SimpleCalendar.cs
@@ -1,7 +1,9 @@
 using System;
 
-public class SimpleCalendar{
-    static void Main(){
+class SimpleCalendar
+{
+    static void Main()
+    {
         Console.Write("Enter month (1-12): ");
         int month = int.Parse(Console.ReadLine());
 
@@ -10,23 +12,26 @@ public class SimpleCalendar{
 
         PrintCalendar(month, year);
     }
-	
+
     // Prints calendar for given month and year
-    static void PrintCalendar(int month, int year){
+    static void PrintCalendar(int month, int year)
+    {
         int days = DateTime.DaysInMonth(year, month);
         DateTime firstDay = new DateTime(year, month, 1);
-		
+
         Console.WriteLine("\nSun Mon Tue Wed Thu Fri Sat");
-		
+
         // Print spaces before first day
-        for (int i = 0; i < (int)firstDay.DayOfWeek; i++){
+        for (int i = 0; i < (int)firstDay.DayOfWeek; i++)
+        {
             Console.Write("    ");
         }
-		
+
         // Print all days
-        for (int day = 1; day <= days; day++){
+        for (int day = 1; day <= days; day++)
+        {
             Console.Write($"{day,3} ");
-			
+
             if ((day + (int)firstDay.DayOfWeek) % 7 == 0)
                 Console.WriteLine();
         }
