7498994a7336b1393eff648db4e64b91503844d5
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-28 23:24:26 +0530

    Add. Program to perform date arithmetic operation

---
+++ core-csharp-practice/gcr-codebase/c# built-in-functions/level-1/DateArithmetic.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class DateArithmetic{
+    static void Main(){
+		
+        Console.Write("Enter a date (yyyy-MM-dd): ");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+
+        DateTime result = date.AddDays(7)
+                              .AddMonths(1)
+                              .AddYears(2)
+                              .AddDays(-21);
+
+        Console.WriteLine("Final Date: " + result.ToShortDateString());
+    }
+}
