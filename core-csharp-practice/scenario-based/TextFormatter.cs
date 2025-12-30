b0372ebe74da581d31c6a8928dd25f09059da50b
aditisingh1528 <aditisingh1528@gmail.com>
2025-12-30 11:15:01 +0530

    Add. Text Formatter to normalize spacing, punctuation, and sentence capitalization

---
+++ core-csharp-practice/scenario-based/TextFormatter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class TextFormatter{
+    static string Format(string text){
+        string formatted = ""; // Stores the final formatted string
+        bool SentenceStart = true;
+
+        for (int i = 0; i < text.Length; i++){
+            char character = text[i];
+            if (character == ' '){ //skipping extra spaces
+                if (formatted.Length == 0 || formatted[formatted.Length - 1] == ' '){ // If string is empty or last char is also space, skip
+                    continue;
+                }
+
+                formatted += ' ';
+                continue;
+            }
+            if (SentenceStart && character >= 'a' && character <= 'z'){ // Capitalize first letter of a sentence
+				
+                character = (char)(character - 32);
+                SentenceStart = false;
+            }
+            if (character == '.' || character == '?' || character == '!' || character == ','){ //handles punctuation marks
+                formatted += character;
+                SentenceStart = true;
+
+                if (i + 1 < text.Length && text[i + 1] != ' '){
+                    formatted += ' ';
+                }
+
+                continue;
+            }
+			
+            formatted += character; //add normal characters
+        }
+		
+        if (formatted.Length > 0 && formatted[formatted.Length - 1] == ' '){ 
+            formatted = formatted.Substring(0, formatted.Length - 1);
+        }
+
+        return formatted;
+    }
+
+    static void Main(string[] args){
+		
+        Console.WriteLine("Enter your text:");
+        string text = Console.ReadLine();
+
+        Console.WriteLine(Format(text));
+    }
+}
