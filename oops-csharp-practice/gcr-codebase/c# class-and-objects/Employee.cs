38ff9acc4e9efb00a3d5b285ffe5a15438e91c96
aditisingh1528 <aditisingh1528@gmail.com>
2026-01-01 16:15:40 +0530

    [Aditi] Add. Programs using different classes and objects

---
+++ oops-csharp-practice/gcr-codebase/c# class-and-objects/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Employee{
+
+    public string Name;
+    public int Id;
+    public double Salary;
+
+    public void DisplayDetails(){
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Id: " + Id);
+        Console.WriteLine("Salary: " + Salary);
+    }
+}
+
+class Program{
+
+    static void Main(){
+
+        // creating employee object
+        Employee emp = new Employee();
+
+        // assigning values
+        emp.Name = "Maxine";
+        emp.Id = 7244;
+        emp.Salary = 90000;
+
+        // displaying details
+        emp.DisplayDetails();
+    }
+}
