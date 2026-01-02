076d3a818f032691dd3c406782365509737a2fd2
aditisingh1528 <aditisingh1528@gmail.com>
2026-01-02 14:40:40 +0530

    Update Circle class with user input and calculations

--- core-csharp-practice/gcr-codebase/c#constructor-instance-access-modifiers/level-1/CircleClass.cs
+++ core-csharp-practice/gcr-codebase/c#constructor-instance-access-modifiers/level-1/CircleClass.cs
@@ -4,10 +4,10 @@ public class Circle
 {
     private double radius;
 
-    // Default constructor
+    //Default constructor
     public Circle() : this(1.0) { }
 
-    // Parameterized constructor
+    //Parameterized constructor
     public Circle(double radius)
     {
         this.radius = radius;
