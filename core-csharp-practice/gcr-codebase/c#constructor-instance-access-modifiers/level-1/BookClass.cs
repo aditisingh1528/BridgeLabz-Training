05c17e19a3304ab0ec2797a4c9c65c0fe2054fc7
aditisingh1528 <aditisingh1528@gmail.com>
2026-01-02 12:16:54 +0530

    Add. Book class with default and parameterized constructors

---
+++ core-csharp-practice/gcr-codebase/c#constructor-instance-access-modifiers/level-1/BookClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Book
+{
+    private string title;
+    private string author;
+    private double price;
+
+    // Default constructor
+    public Book()
+    {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0;
+    }
+
+    // Parameterized constructor
+    public Book(string title, string author, double price)
+    {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{title} | {author} | ₹{price}");
+    }
+
+    static void Main()
+    {
+        Book b1 = new Book();
+        Book b2 = new Book("Clean Code", "Robert Martin", 500);
+
+        b1.Display();
+        b2.Display();
+    }
+}
