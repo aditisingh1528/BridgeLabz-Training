1bdd18439c836d81d7d2c421da3afaa71aa977d2
aditisingh1528 <aditisingh1528@gmail.com>
2026-01-13 16:56:14 +0530

    [Aditi] Add. Maintaining a personal digital bookshelf by adding, updating, and sorting favourite books

---
+++ oops-csharp-practice/scenario-based/BookBuddy/Book.cs
@@ -0,0 +1,29 @@
+﻿public class Book
+{
+    // Stores title and author together
+    private string titleAuthor;
+
+    // Constructor to initialize book details
+    public Book(string title, string author)
+    {
+        titleAuthor = title + " - " + author;
+    }
+
+    // Returns only the title part
+    public string GetTitle()
+    {
+        return titleAuthor.Split(" - ")[0];
+    }
+
+    // Returns only the author part
+    public string GetAuthor()
+    {
+        return titleAuthor.Split(" - ")[1];
+    }
+
+    // Returns full book info
+    public string GetBookInfo()
+    {
+        return titleAuthor;
+    }
+}
