f92a5d5e78dc300a12e3ee18137c631aa8e18570
aditisingh1528 <aditisingh1528@gmail.com>
2026-01-19 15:06:21 +0530

    [Aditi] Add. Delivery Chain Mangement- tracking parcels through stages like shipped, in transit, delivered.

---
+++ datastructures-csharp-practice/scenario-based/ParcelTracker/IParcelTracker.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParcelTrackerApp
+{
+    interface IParcelTracker
+    {
+        void AddStage(string StageName);
+        void AddCheckpointAfter(string ExistingStage, string NewStage);
+        void TrackParcel();
+    }
+}
